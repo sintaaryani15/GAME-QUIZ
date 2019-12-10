f83a2451817fc161043aae3d4874e50414fabf17
sintaaryani15 <sintaaryani01@gmail.com>
2019-12-11 00:09:46 +0700

    game kuis matematika

---
+++ Assets/script/tombol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tombol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start () {
+		
+	}
+	public void scene(string scene){
+		Application.LoadLevel (scene);
+	}
+	public void scale(float scale){
+		transform.localScale = new Vector2 (1/scale,1*scale);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
